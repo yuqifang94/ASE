257c49b6972558ca1d243b5934d69b3357897ade
Weiqiang Zhou <kenzhou86@gmail.com>
2021-07-16 10:44:34 -0400

    Update TFBS_mouse_regions.R

--- TFBS_analysis_mouse/TFBS_mouse_regions.R
+++ TFBS_analysis_mouse/TFBS_mouse_regions.R
@@ -8,18 +8,18 @@ options(scipen=999)
 setwd("../downstream/input/mouse_analysis")
 
 ##obtain regulaotry DNA regions from mouse DNase-seq peak data
-metadata <- readRDS("/dcl02/hongkai/data/wzhou14/ENCODE_data/metadata/match_RNA_DNase_mm10.rds")
+metadata <- readRDS("../downstream/input/mouse_analysis/ENCODE_data/metadata/match_RNA_DNase_mm10.rds")
 metadata_filter <- metadata[grep("C57BL/6",metadata$Biosample),]
 colnames(metadata_filter) <- c("sample", "RNA_ID", "DNase_ID")
 
-match_file <- readRDS("/dcl02/hongkai/data/wzhou14/ENCODE_data/metadata/DNase_mm10_file_match.rds")
+match_file <- readRDS("../downstream/input/mouse_analysis/ENCODE_data/metadata/DNase_mm10_file_match.rds")
 colnames(match_file) <- c("DNase_peak", "DNase_ID")
 peak_match <- match_file[match_file$DNase_ID %in% metadata_filter$DNase_ID,]
 
 peak_match <- left_join(peak_match, metadata_filter)
 saveRDS(peak_match, file="DNase_RNA_peak_match_mm10.rds")
 
-filelist <- list.files(path="/dcl02/hongkai/data/wzhou14/ENCODE_data/peak/mm10/DNase_house",pattern="\\.narrowPeak",full.names = TRUE, recursive = TRUE)
+filelist <- list.files(path="../downstream/input/mouse_analysis/ENCODE_data/peak/mm10/DNase_house",pattern="\\.narrowPeak",full.names = TRUE, recursive = TRUE)
 
 filelist_in <- sapply(peak_match[,1],function(x){
   grep(x,filelist, value = TRUE)
@@ -50,7 +50,7 @@ gr_peak_fix_cod$strand <- "+"
 
 write.table(gr_peak_fix_cod, file="DNase_mm10_peak_merge_250bp.cod", col.names=FALSE, sep="\t", quote=FALSE)
 
-system("~/cisgenome_project/bin/refgene_getmatchedcontrol -d /users/wzhou14/cisgenome_project/mm10/annotation/refFlat_sorted.txt -dt 1 -s mouse -c /users/wzhou14/cisgenome_project/mm10/chrlen.txt -i DNase_mm10_peak_merge_250bp.cod -o DNase_mm10_peak_merge_250bp_control.cod -n 1 -l 250 -nr 1")
+system("~/cisgenome_project/bin/refgene_getmatchedcontrol -d ~/cisgenome_project/mm10/annotation/refFlat_sorted.txt -dt 1 -s mouse -c ~/cisgenome_project/mm10/chrlen.txt -i DNase_mm10_peak_merge_250bp.cod -o DNase_mm10_peak_merge_250bp_control.cod -n 1 -l 250 -nr 1")
 
 ##exclude overlapping regions from the control
 peak_control <- fread("DNase_mm10_peak_merge_250bp_control.cod")
