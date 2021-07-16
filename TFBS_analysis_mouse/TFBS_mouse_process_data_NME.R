c89ccd89843e1befe661fef2c1910e4910eb522f
Weiqiang Zhou <kenzhou86@gmail.com>
2021-07-16 10:46:14 -0400

    Update TFBS_mouse_process_data_NME.R

--- TFBS_analysis_mouse/TFBS_mouse_process_data_NME.R
+++ TFBS_analysis_mouse/TFBS_mouse_process_data_NME.R
@@ -3,12 +3,12 @@ library(dplyr)
 library(glue)
 library(matrixStats)
 
-setwd("/dcl02/hongkai/data/wzhou14/Andy_lab/yuqi_project/mouse_TFBS_analysis")
+setwd("../downstream/output/mouse_analysis/TFBS_analysis")
 
 ##read in NME data for regulatory DNA regions and calculate the median NME for each motif
 data_median_DNase <- lapply(c(1:12), function(x) {
 
-	data_temp <- readRDS(glue("/dcl02/hongkai/data/wzhou14/Andy_lab/share/Mouse_NME_all/DNase/JASPAR_motif_mm10_NME_{x}_agnostic_merged_DNase.rds"))
+	data_temp <- readRDS(glue("../downstream/input/mouse_analysis/TFBS_analysis/JASPAR_motif_mm10_NME_{x}_agnostic_merged_DNase.rds"))
 	data_temp_median <- sapply(data_temp, function(y){
 		data_mat <- as.matrix(mcols(y))
 		data_mat_median <- colMedians(data_mat,na.rm = TRUE)
@@ -26,7 +26,7 @@ saveRDS(data_median_com_DNase, file="mouse_motif_DNase_median.rds")
 ##read in NME data for non-regulatory DNA regions and calcualte the median NME for each motif
 data_median_control <- lapply(c(1:12), function(x) {
 
-	data_temp <- readRDS(glue("/dcl02/hongkai/data/wzhou14/Andy_lab/share/Mouse_NME_all/control/JASPAR_motif_mm10_NME_{x}_agnostic_merged_control.rds"))
+	data_temp <- readRDS(glue("../downstream/input/mouse_analysis/TFBS_analysis/JASPAR_motif_mm10_NME_{x}_agnostic_merged_control.rds"))
 	data_temp_median <- sapply(data_temp, function(y){
 		data_mat <- as.matrix(mcols(y))
 		data_mat_median <- colMedians(data_mat,na.rm = TRUE)
