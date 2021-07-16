41410288d51220de468dbda74b79946ad60ebfc4
Weiqiang Zhou <kenzhou86@gmail.com>
2021-07-16 10:35:56 -0400

    Update TFBS_human_process_data_MML.R

--- TFBS_analysis_human/TFBS_human_process_data_MML.R
+++ TFBS_analysis_human/TFBS_human_process_data_MML.R
@@ -3,12 +3,12 @@ library(dplyr)
 library(glue)
 library(matrixStats)
 
-setwd("/dcl02/hongkai/data/wzhou14/Andy_lab/yuqi_project/human_TFBS_analysis")
+setwd("../downstream/output/human_analysis/TFBS_analysis")
 
 ##read in MML data for regulatory DNA regions and calculate the median MML for each motif
 data_median_DNase <- lapply(c(1:3), function(x) {
 
-	data_temp <- readRDS(glue("/dcl02/hongkai/data/wzhou14/Andy_lab/share/NME_motif_DNase_hg19/JASPAR_motif_hg19_MML_{x}_agnostic_DNase.complete.rds"))
+	data_temp <- readRDS(glue("../downstream/input/human_analysis/TFBS_analysis/JASPAR_motif_hg19_MML_{x}_agnostic_DNase.complete.rds"))
 	data_temp_median <- sapply(data_temp, function(y){
 		data_mat <- as.matrix(mcols(y))
 		data_mat_median <- colMedians(data_mat,na.rm = TRUE)
@@ -26,7 +26,7 @@ saveRDS(data_median_com_DNase, file="human_motif_DNase_median_MML.rds")
 ##read in MML data for non-regulatory DNA regions and calcualte the median MML for each motif
 data_median_control <- lapply(c(1:3), function(x) {
 
-	data_temp <- readRDS(glue("/dcl02/hongkai/data/wzhou14/Andy_lab/share/NME_motif_DNase_hg19/JASPAR_motif_hg19_MML_{x}_agnostic_Control.complete.rds"))
+	data_temp <- readRDS(glue("../downstream/input/human_analysis/TFBS_analysis/JASPAR_motif_hg19_MML_{x}_agnostic_Control.complete.rds"))
 	data_temp_median <- sapply(data_temp, function(y){
 		data_mat <- as.matrix(mcols(y))
 		data_mat_median <- colMedians(data_mat,na.rm = TRUE)
