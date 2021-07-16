db80a1872ece949c72178b17fbab3cb8f69fb934
Weiqiang Zhou <kenzhou86@gmail.com>
2021-07-16 10:31:03 -0400

    Update TFBS_human_regions.R

--- TFBS_analysis_human/TFBS_human_regions.R
+++ TFBS_analysis_human/TFBS_human_regions.R
@@ -2,11 +2,11 @@ library(GenomicRanges)
 library(data.table)
 options(scipen=999)
 
-setwd("/dcl02/hongkai/data/wzhou14/Andy_lab/yuqi_project/TFBS_analysis")
+setwd("../downstream/output/human_analysis/TFBS_analysis")
 
 ##read in DNase regions (i.e., regulatory DNA regions)
 ##DNase regions were obtained from https://github.com/WeiqiangZhou/BIRD-data/releases/download/v3.0/BIRD_data_ENCODE.zip and resize to 250bp for each region.
-DNase_region <- readRDS("/dcl02/hongkai/data/wzhou14/Andy_lab/share/DNase_hg19_250bp.rds")
+DNase_region <- readRDS("../downstream/input/human_analysis/DNase_hg19_250bp.rds")
 
 ##run cisgenome to get control regions (i.e., non-regulatory DNA regions)
 gr_peak_fix_cod <- data.frame(DNase_region)[,-4]
@@ -14,7 +14,7 @@ gr_peak_fix_cod$strand <- "+"
 
 write.table(gr_peak_fix_cod, file="DNase_hg19_250bp.cod", col.names=FALSE, sep="\t", quote=FALSE)
 
-system("~/cisgenome_project/bin/refgene_getmatchedcontrol -d /users/wzhou14/cisgenome_project/hg19/annotation/refFlat_sorted.txt -dt 1 -s human -c /users/wzhou14/cisgenome_project/hg19/chrlen.txt -i DNase_hg19_250bp.cod -o DNase_hg19_250bp_control.cod -n 1 -l 250 -nr 1")
+system("~/cisgenome_project/bin/refgene_getmatchedcontrol -d ~/cisgenome_project/hg19/annotation/refFlat_sorted.txt -dt 1 -s human -c ~/cisgenome_project/hg19/chrlen.txt -i DNase_hg19_250bp.cod -o DNase_hg19_250bp_control.cod -n 1 -l 250 -nr 1")
 
 ##exclude overlapping regions from the control
 peak_control <- fread("DNase_hg19_250bp_control.cod")[-1,]
