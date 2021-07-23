1c0f95b558b0553a5a4b386650952583bdc9a519
yuqi.fang <34284407+yuqifang94@users.noreply.github.com>
2021-07-23 11:03:47 -0400

    update path

--- cluster_non_tissue_specific.R
+++ cluster_non_tissue_specific.R
@@ -1,2 +1,68 @@
 source('mainFunctions_sub.R')
+cut <- 0.1
+UC_in_matrix_ls=readRDS(UC_in_matrix_ls_file)
+# $EFP
+# [1] 5008585
+# $forebrain
+# [1] 5005549
+# $heart
+# [1] 5006020
+# $hindbrain
+# [1] 5006806
+# $limb
+# [1] 5006777
+# $liver
+# [1] 4995202
+# $midbrain
+# [1] 5004404
+for (seed in 1:10) {
+  d <- lapply(UC_in_matrix_ls,convert_GR,direction='matrix')
+  timeorder <- sapply(1:20,function(i) paste0('E',i,'.5-E',i+1,'.5'))
+  
+  aid <- sapply(names(d),function(i) {
+    names(which(rowSums(d[[i]] > cut) > 0))
+  })  
+#   $EFP
+# [1] 951833
+# $forebrain
+# [1] 1184078
+# $heart
+# [1] 1007666
+# $hindbrain
+# [1] 1149669
+# $limb
+# [1] 988370
+# $liver
+# [1] 1111038
+# $midbrain
+# [1] 1129165
+  d <- sapply(names(d),function(i) {
+    #This is for one and only one
+    #sid <- setdiff(aid[[i]],unlist(aid[names(aid)!=i]))
+    sid=aid[[i]]
+    i <- d[[i]]
+    colnames(i)=sub('-all','',sub('.*?-','',colnames(i)))
+    i <- i[sid,]
+    i <- i[,colnames(i) %in% timeorder]
+    i <- scalematrix(i)
+    i <- i[complete.cases(i),]
+    set.seed(seed)
+    clu <- kmeans(i,10,iter.max = 10000)$cluster
+    n <- names(clu)
+    clum <- rowsum(i,clu)/as.vector(table(clu))
+    maxp <- apply(clum,1,function(i) {
+      i[i < 0] <- 0
+      i <- i-min(i)
+      i <- i/max(i)
+      sum(i*c(1:length(i)))/sum(i)
+    })
+    clu <- rank(maxp)[clu]
+    names(clu) <- n
+    clu
+  })
+  saveRDS(d,file=paste0(dir_cluster_in,'uc_',cut,'_',seed,'.rds'))
+}
+
+
+
 
