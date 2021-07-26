f56ad4d92bdf9ba1c6d9edd71b862e64f4510e0a
yuqi.fang <34284407+yuqifang94@users.noreply.github.com>
2021-07-26 13:45:14 -0400

    update

--- data_collection/coverage_calc.sh
+++ data_collection/coverage_calc.sh
@@ -2,9 +2,10 @@
 #SBATCH -J coverage_calc
 #SBATCH --partition=shared,parallel,skylake
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=24
-#SBATCH -o coverage_logfile/coverage-%A.out
-#SBATCH -e coverage_logfile/coverage-%A.err
+#SBATCH --cpus-per-task=4
+#SBATCH -o ../../downstream/output/mouse_analysis/data_collection_log/coverage-%A.out
+#SBATCH -e ../../downstream/output/mouse_analysis/data_collection_log/coverage-%A.err
+#bedtools v2.29.2
 bed_in=$1
 bam_in=$2
 cov_out=$3
