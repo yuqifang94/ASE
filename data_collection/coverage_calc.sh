0c5d979f676a4d3ed8ba6928d3b115029a8a65cf
yuqi.fang <34284407+yuqifang94@users.noreply.github.com>
2021-07-26 14:20:52 -0400

    update

--- data_collection/coverage_calc.sh
+++ data_collection/coverage_calc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J coverage_calc
 #SBATCH --partition=shared,parallel,skylake
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH -o ../../downstream/output/mouse_analysis/data_collection_log/coverage-%A.out
 #SBATCH -e ../../downstream/output/mouse_analysis/data_collection_log/coverage-%A.err
