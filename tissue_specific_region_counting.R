7b4c84a25b717dc58f973d1d100ad4774842ea99
yuqi.fang <34284407+yuqifang94@users.noreply.github.com>
2021-09-08 12:10:35 -0400

    update ui

--- tissue_specific_region_counting.R
+++ tissue_specific_region_counting.R
@@ -1172,17 +1172,17 @@ for(ts in names(dNME_in)){
 saveRDS(list(UC_cutoff=lapply(UC_cutoff,assign_name,step_in=step),
             dMML_cutoff=lapply(dMML_cutoff,assign_name,step_in=step),
             dNME_cutoff=lapply(dNME_cutoff,assign_name,step_in=step)
-            ),'../downstream/output/mouse_analysis/UC_dNME_olap/regions_non_ts_only_001.rds')
+            ),'../downstream/output/mouse_analysis/UC_dNME_olap/regions_non_ts_only_01.rds')
 
 
-cutoff_ts_all_out=readRDS('../downstream/output/mouse_analysis/UC_dNME_olap/regions_non_ts_only.rds')
+cutoff_ts_all_out=readRDS('../downstream/output/mouse_analysis/UC_dNME_olap/regions_non_ts_only_001.rds')
 
 cutoff_ts_all_out=list(UC_cutoff=lapply(UC_cutoff,assign_name,step_in=step),
             dMML_cutoff=lapply(dMML_cutoff,assign_name,step_in=step),
             dNME_cutoff=lapply(dNME_cutoff,assign_name,step_in=step))
 cutoff_ts_num_output_all=list()
 set.seed(12345)
- pdf(paste0('../downstream/output/mouse_analysis/UC_dNME_olap/UC_dNME_dMML_cutoff_num_non_ts_only_001.pdf'))
+pdf(paste0('../downstream/output/mouse_analysis/UC_dNME_olap/UC_dNME_dMML_cutoff_num_non_ts_only_001.pdf'))
 for(ts in names(region_rand_pool)){
     
     tt1=proc.time()[[3]]
@@ -1198,9 +1198,16 @@ for(ts in names(region_rand_pool)){
     print(ggplot(ts_num_dt_mt,aes(x=cutoff,y=region_count,color=stat_type))+geom_line()+
             ylab("Number of regions")+ggtitle(ts))
     cat("Processing:",ts,'\n')
-    cutoff_ts_num_output=as.data.table(expand.grid(seq(0,max(ts_num_dt[UC>=round(mean(ts_num_dt$UC)/100,digits=2)]$cutoff),step*5),
-                                        seq(0,max(ts_num_dt[dNME>=round(mean(ts_num_dt$dNME)/100,digits=2)]$cutoff),step*10),
-                                        seq(0,max(ts_num_dt[dMML>=round(mean(ts_num_dt$dMML)/100,digits=2)]$cutoff),step*10)))
+    #Expand grid based on number of regions
+    # cutoff_ts_num_output=as.data.table(expand.grid(seq(0,max(ts_num_dt[UC>=round(mean(ts_num_dt$UC)/100,digits=2)]$cutoff),step),
+    #                                     seq(0,max(ts_num_dt[dNME>=round(mean(ts_num_dt$dNME)/100,digits=2)]$cutoff),step*5),
+    #                                     seq(0,max(ts_num_dt[dMML>=round(mean(ts_num_dt$dMML)/100,digits=2)]$cutoff),step*5)))
+    num=100
+    num_dMML_dNME=20
+    cutoff_ts_num_output=as.data.table(expand.grid( ts_num_dt[unlist(lapply( quantile(ts_num_dt$UC,seq(0,1,1/num)),function(x) which.min(abs(ts_num_dt$UC-x))))]$cutoff,
+                                                    ts_num_dt[unlist(lapply( quantile(ts_num_dt$dNME,seq(0,1,1/num_dMML_dNME)),function(x) which.min(abs(ts_num_dt$dNME-x))))]$cutoff,
+                                                    ts_num_dt[unlist(lapply( quantile(ts_num_dt$dMML,seq(0,1,1/num_dMML_dNME)),function(x) which.min(abs(ts_num_dt$dMML-x))))]$cutoff
+                                                        ))
     colnames(cutoff_ts_num_output)=c("UC","dNME","dMML")
 
     cutoff_ts_num_output_all[[ts]]=
@@ -1213,6 +1220,8 @@ for(ts in names(region_rand_pool)){
                                                 UC_region=cutoff_ts_all_out_ts$UC_cutoff[[as.character(cutoff_ts_num_output$UC[[x]])]]
                                                 #if(length(UC_region)>0){
                                                 UC_region_rand=sample(region_rand_pool[[ts]],length(UC_region))
+                                                dMML_region_rand=sample(region_rand_pool[[ts]],length(dMML_region))
+                                                dNME_region_rand=sample(region_rand_pool[[ts]],length(dNME_region))
                                                 return(data.table(        
                                                     dNME_specific=length(setdiff(dNME_region,UC_region)),
                                                     dMML_specific=length(setdiff(dMML_region,UC_region)),
@@ -1220,12 +1229,21 @@ for(ts in names(region_rand_pool)){
                                                     UC_specific_dMML=length(setdiff(UC_region,dMML_region)),
                                                     shared_dNME=length(intersect(dNME_region,UC_region)),
                                                     shared_dMML=length(intersect(dMML_region,UC_region)),
-                                                    dNME_specific_rand=length(setdiff(dNME_region,UC_region_rand)),
-                                                    dMML_specific_rand=length(setdiff(dMML_region,UC_region_rand)),
-                                                    UC_specific_dNME_rand=length(setdiff(UC_region_rand,dNME_region)),
-                                                    UC_specific_dMML_rand=length(setdiff(UC_region_rand,dMML_region)),
-                                                    shared_dNME_rand=length(intersect(dNME_region,UC_region_rand)),
-                                                    shared_dMML_rand=length(intersect(dMML_region,UC_region_rand)),
+                                                    UC_rand_dNME_specific=length(setdiff(dNME_region,UC_region_rand)),
+                                                    UC_rand_dMML_specific=length(setdiff(dMML_region,UC_region_rand)),
+                                                    UC_rand_UC_specific_dNME=length(setdiff(UC_region_rand,dNME_region)),
+                                                    UC_rand_UC_specific_dMML=length(setdiff(UC_region_rand,dMML_region)),
+                                                    UC_rand_shared_dNME=length(intersect(dNME_region,UC_region_rand)),
+                                                    UC_rand_shared_dMML=length(intersect(dMML_region,UC_region_rand)),
+
+                                                    dNME_rand_dNME_specific=length(setdiff(dNME_region_rand,UC_region)),
+                                                    dNME_rand_UC_specific_dNME=length(setdiff(UC_region,dNME_region_rand)),
+                                                    dNME_rand_shared_dNME=length(intersect(dNME_region_rand,UC_region)),
+
+                                                    dMML_rand_dMML_specific=length(setdiff(dMML_region_rand,UC_region)),
+                                                    dMML_rand_UC_specific_dMML=length(setdiff(UC_region,dMML_region_rand)),
+                                                    dMML_rand_shared_dMML=length(intersect(dMML_region_rand,UC_region)),
+
                                                     dNME_cutoff=cutoff_ts_num_output$dNME[[x]],
                                                     dMML_cutoff=cutoff_ts_num_output$dMML[[x]],
                                                     UC_cutoff=cutoff_ts_num_output$UC[[x]]
