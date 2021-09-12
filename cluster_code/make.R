026a0ff700044c79182113a0bf61c2f79a17200c
yuqi.fang <34284407+yuqifang94@users.noreply.github.com>
2021-09-12 17:33:16 -0400

    update

--- cluster_code/make.R
+++ cluster_code/make.R
@@ -1,7 +1,7 @@
 library(RColorBrewer)
 library(pheatmap)
 setwd('../')
-source('mainFunction_sub.R')
+source('mainFunctions_sub.R')
 cut <- as.numeric(commandArgs(trailingOnly = T))
 for (seed in 1:10) {
   #see fulluc.R
