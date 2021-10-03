34e253dd8ee49dabb9a64d0bb727c4aa5b78a478
yuqi.fang <34284407+yuqifang94@users.noreply.github.com>
2021-10-03 14:05:51 -0400

    update cwd

--- data_IO/bedGraph_to_bw.sh
+++ data_IO/bedGraph_to_bw.sh
@@ -6,6 +6,7 @@
 #$ -m ebas
 #$ -M "yfang27@jhmi.edu"
 #$ -N bedGraph-bw
+#$ -cwd
 # Args
 bedGraph_in="$1"
 output_dir="$2"
@@ -17,3 +18,6 @@ bw_out=${fn/\.bedGraph/.bw}
 # Cut bedfile for first row (data)
 echo cut -f1,2,3,4 $bedGraph_in > $intermediate_dir$fn.cut
 echo bedGraphToBigWig $intermediate_dir$fn.cut $chrom_size_file $output_dir$bw_out
+
+#Example submission
+#qsub data_IO/bedGraph_to_bw.sh ../data_submission/compliment_MML_NME_model_mouse/mm10_heart_day10_5_all_allele_agnostic_mml.bedGraph ../data_submission/compliment_MML_NME_model_mouse/ ../data_submission/bedGraph_to_bw_intermediate/ ../data_submission/mm10.chrom.size
