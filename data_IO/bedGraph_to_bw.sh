5a79b290eb5c4fb4eaa658ea2dd11004927f7635
Yuqi Fang <yqf.fang@gmail.com>
2021-10-07 14:50:47 -0400

    remove ch

--- data_IO/bedGraph_to_bw.sh
+++ data_IO/bedGraph_to_bw.sh
@@ -16,8 +16,8 @@ fn=${bedGraph_in/*\//}
 bw_out=${fn/\.bedGraph/.bw}
 
 # Cut bedfile for first row (data)
-echo cut -f1,2,3,4 $bedGraph_in > $intermediate_dir$fn.cut
-echo bedGraphToBigWig $intermediate_dir$fn.cut $chrom_size_file $output_dir$bw_out
+cut -f1,2,3,4 $bedGraph_in > $intermediate_dir$fn.cut
+bedGraphToBigWig $intermediate_dir$fn.cut $chrom_size_file $output_dir$bw_out
 
 #Example submission
 #qsub data_IO/bedGraph_to_bw.sh ../data_submission/compliment_MML_NME_model_mouse/mm10_heart_day10_5_all_allele_agnostic_mml.bedGraph ../data_submission/compliment_MML_NME_model_mouse/ ../data_submission/bedGraph_to_bw_intermediate/ ../data_submission/mm10.chrom.size
